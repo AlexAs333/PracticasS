d6a820ebc9ef78fe0d433827cda716642a5fa28e
AlexAs33 <alexasensioboj@gmail.com>
2024-04-30 12:18:03 +0200

    p5 Tprog def

--- practica5_XXXXXX/LSystem.hs
+++ practica5_XXXXXX/LSystem.hs
@@ -5,26 +5,14 @@ import SVG
 import Turtle
 import Rules
 
--------------------TAREA 1-------------------
-tplot :: Turtle -> String -> [Position]
-tplot (_, _, pos, _) [] = [pos]  -- Caso Base --> Hemos recorrido todo el string
-tplot trotuman (s:ss) = pos trotuman : tplot (moveTurtle trotuman action) ss  -- Caso Recursivo
-    where
-        pos (_, _, p, _) = p
-        action = case s of
-            '>' -> Forward
-            '+' -> TurnRight
-            '-' -> TurnLeft
-
--------------------TAREA 2-------------------
 --Devuelve String de movimientos
 lsystem :: (Char -> String) -> String -> Int -> String
 lsystem rule s 0 = s -- Caso Base --> Ya se ha hecho todas las veces
 lsystem rule s n = lsystem rule (concat (map rule s)) (n - 1) -- Caso recursivo             
 
-tplot2 :: Turtle -> String -> [Position]
-tplot2 (_, _, pos, _) [] = [pos]  -- Caso Base --> Hemos recorrido todo el string
-tplot2 trotuchad (s:ss) = pos trotuchad : tplot2 newTrotuchad ss  -- Caso Recursivo
+tplot :: Turtle -> String -> [Position]
+tplot (_, _, pos, _) [] = [pos]  -- Caso Base --> Hemos recorrido todo el string
+tplot trotuchad (s:ss) = pos trotuchad : tplot newTrotuchad ss  -- Caso Recursivo
     where
         pos (_, _, p, _) = p
         newTrotuchad = case s of
--- practica5_XXXXXX/main.hs
+++ practica5_XXXXXX/main.hs
@@ -26,45 +26,45 @@ main = do
 -------------------PRUEBAS DIBUJOS-------------------
     --Arrow Head
     let arrowHead = lsystem rulesArrowHead "F" 2
-    let posArrowHead = tplot2 (1, 40, (0,0), 60) arrowHead
+    let posArrowHead = tplot (1, 40, (0,0), 60) arrowHead
     savesvg "ArrowHead" posArrowHead
 
     --Curva de Koch
     let koch = lsystem rulesKoch "Fkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk" 3
-    let posKoch = tplot2 (1, 60, (0,0), 0) koch
+    let posKoch = tplot (1, 60, (0,0), 0) koch
     savesvg "CurvaDeKoch" posKoch
 
     --Curva de Koch cuadrada
     let cuadrada = lsystem rulesKochCuadrada "F" 3
-    let posCuadrada = tplot2 (1, 90, (0,0), 0) cuadrada
+    let posCuadrada = tplot (1, 90, (0,0), 0) cuadrada
     savesvg "CurvaKochCuadrada" posKoch
 
     --Koch Snowflake
     let snow = lsystem rulesSnowflake "F++khF++Fkkkk" 3
-    let posSnow = tplot2 (1, 60, (0,0), 0) snow
+    let posSnow = tplot (1, 60, (0,0), 0) snow
     savesvg "KochSnowflake" posSnow
 
     --Isla de Minkowski
     let isla = lsystem rulesIsla "F+F+F+F" 2
-    let posIsla = tplot2 (1, 90, (0,0), 0) isla
+    let posIsla = tplot (1, 90, (0,0), 0) isla
     savesvg "IslaMinkowski" posIsla
 
     --Sierpinsky Arrow Head
     let sierpinskyarrow = lsystem rulessFlecha "F" 6
-    let posflecha = tplot2 (1, 60, (0,0), 0) sierpinskyarrow
+    let posflecha = tplot (1, 60, (0,0), 0) sierpinskyarrow
     savesvg "SierpinskyArrowHead" posflecha
     
     --Sierpinsky
     let sierpinsky= lsystem rulesSierpinsky"F-G-G" 4
-    let possierpinsky = tplot2 (1, 120, (0,0), 180) sierpinsky
+    let possierpinsky = tplot (1, 120, (0,0), 180) sierpinsky
     savesvg "TrianguloSierpinsky" possierpinsky
 
     --Curva de Hilbert
     let hilbert= lsystem rulesHilbert "F" 4
-    let poshilbert = tplot2 (1, 90, (0,0), 180) hilbert
+    let poshilbert = tplot (1, 90, (0,0), 180) hilbert
     savesvg "CurvaDeHilbert" poshilbert
 
     --Curva de Gosper
     let gosper = lsystem rulesGosper "F" 3
-    let posgosper = tplot2 (1, 90, (0,0), 135) gosper
+    let posgosper = tplot (1, 90, (0,0), 135) gosper
     savesvg "CurvaDeGosper" posgosper
