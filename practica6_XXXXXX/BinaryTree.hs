2b644f056e7967d3b2110f806506cfe7ec56f0a8
AlexAs333 <161581616+AlexAs333@users.noreply.github.com>
2024-05-04 19:08:50 +0200

    Tprog warnings

--- practica6_XXXXXX/BinaryTree.hs
+++ practica6_XXXXXX/BinaryTree.hs
@@ -1,5 +1,7 @@
 module BinaryTree where
 
+import Data.List
+
 --Definición tipos de datos
 data Arbol a = Nodo a (Arbol a) (Arbol a) | Empty
 
@@ -49,3 +51,52 @@ build' [] arbol = arbol     --Caso base
 build' (x:xs) arbol = build' xs arbol'  --Caso Recursivo
     where
         arbol' = add arbol x
+
+---------------FUNCIONES DEL ÁRBOL BINARIO DE BÚSQUEDA EQUILIBRADO (AVL)---------------
+buildBalanced :: (Ord a) => [a] -> Arbol a
+buildBalanced l = buildBalanced' lOrd empty
+    where
+        lOrd = sort l
+
+buildBalanced' :: (Ord a) => [a] -> Arbol a -> Arbol a
+buildBalanced' [] arbol = arbol
+buildBalanced' l arbol = arbol'
+    where
+        (medio, listaIzq, listaDech) = divideLista l
+        arbol' = Nodo medio (buildBalanced' listaIzq empty) (buildBalanced' listaDech empty)
+
+balance :: (Ord a) => Arbol a -> Arbol a
+balance arbol = buildBalanced l
+    where
+        l = arbolLista arbol
+
+divideLista :: [a] -> (a, [a], [a])
+divideLista [] = error "La lista está vacía"
+divideLista [x] = (x, [], [])
+divideLista xs = (mid, left, right)
+  where
+    mid = head (drop (length xs `div` 2) xs)
+    left = take (length xs `div` 2) xs
+    right = drop ((length xs `div` 2) + 1) xs
+
+arbolLista :: Arbol a -> [a]
+arbolLista Empty = []
+arbolLista (Nodo x izq dch) = arbolLista izq ++ [x] ++ arbolLista dch
+
+---------------RECORRIDO DE ÁRBOLES BINARIOS---------------
+preorder :: Arbol a -> [a]
+preorder Empty = []
+preorder (Nodo x izq dch) = [x] ++ preorder izq ++ preorder dch
+
+inorder :: Arbol a -> [a]
+inorder Empty = []
+inorder (Nodo x izq dch) =  inorder izq ++ [x] ++ inorder dch
+
+postorder :: Arbol a -> [a]
+postorder Empty = []
+postorder (Nodo x izq dch) =  postorder izq ++ postorder dch ++ [x]
+
+between :: (Ord a) => Arbol a -> a -> a -> [a]
+between arbol min max = filter (\x -> x == min || (x == max) || (x > min && x < max)) aLista
+    where
+        aLista = arbolLista arbol
